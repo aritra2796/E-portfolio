40e0dead2842d6c2686545513a5ac7972e4362a6
asus <aritra2796@gmail.com>
2021-02-26 04:59:34 +0530

    login

--- config/app.js
+++ config/app.js
@@ -24,7 +24,7 @@ mongodb.once('open', => {
   console.log('connected to database');
 })*/
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
