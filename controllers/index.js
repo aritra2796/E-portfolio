e5d4e1002c3fa65d70f3b923b5044357174e262a
asus <aritra2796@gmail.com>
2021-03-01 05:37:25 +0530

    login

--- controllers/index.js
+++ controllers/index.js
@@ -10,8 +10,8 @@ Feedback.message = null;
 
 /* GET home page. */
 module.exports.displayHomePage = (req, res, next) => {
-  res.render('pages/home', {
-    title: 'Home', 
+  res.render('/', {
+    title: 'E-portfolio', 
     contact:Feedback,
     displayName: req.user ? req.user.displayName : ''
   });
@@ -19,18 +19,18 @@ module.exports.displayHomePage = (req, res, next) => {
 
 /* GET about page. */
 module.exports.displayAboutPage = (req, res, next) => {
-  res.render('pages/about', { title: 'About',
+  res.render('/about', { title: 'About',
   displayName: req.user ? req.user.displayName : '' });
 };
 
 /* GET projects page. */
 module.exports.displayProjectPage =(req, res, next) => {
-  res.render('pages/projects', { title: 'Projects',
+  res.render('/projects', { title: 'Projects',
   displayName: req.user ? req.user.displayName : '' });
 };
 
 /* GET Services page. */
 module.exports.displayServicesPage = (req, res, next) => {
-  res.render('pages/services', { title: 'Services',
+  res.render('/services', { title: 'Services',
   displayName: req.user ? req.user.displayName : '' });
 };
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,6 @@ let indexController = require('../controllers/index');
 /* GET home page. */
 router.get('/', indexController.displayHomePage);
 
-/* GET home page. */
-router.get('/home', indexController.displayHomePage);
-
 /* GET about page. */
 router.get('/about',  indexController.displayAboutPage);
 
