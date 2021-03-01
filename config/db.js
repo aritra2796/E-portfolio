d46f7d17d8c1585d25a8f9f421bcf31fa5e550a0
asus <aritra2796@gmail.com>
2021-03-01 06:19:22 +0530

    login

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "URI": "mongodb://",
+    "URI": "mongodb://mongodb+srv://aritra:1234@cluster0.c2mnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     "secret": 'SomeSecret'
 }
